076595cf33cb3520a7982ec7c4cf9bd14c897680
gilles <meyomesse.gilles@gmail.com>
2017-01-12 17:05:17 +0100

    [MIG] migrate qweb_usertime from 8 to 9

--- qweb_usertime/models/ir_qweb.py
+++ qweb_usertime/models/ir_qweb.py
@@ -1,24 +1,6 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
 #    This module copyright (C) 2015 Savoir-faire Linux
 #    (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 """ QWeb usertime : adds support for t-usertime on qweb reports """
 
@@ -32,7 +14,7 @@ from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
 _logger = logging.getLogger(__name__)
 
 
-class QWeb(models.Model):
+class IrQweb(models.Model):
     _inherit = 'ir.qweb'
 
     def render_tag_usertime(self, element, template_attributes,
