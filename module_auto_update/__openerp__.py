7b25b0da7a79ff79fa333108df8d61afde886196
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-03-12 19:09:29 +0100

    [MIG] module_auto_update: adapt to new checksum mechanism

--- module_auto_update/__openerp__.py
+++ module_auto_update/__openerp__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Module Auto Update',
     'summary': 'Automatically update Odoo modules',
-    'version': '9.0.1.1.0',
+    'version': '9.0.2.0.0',
     'category': 'Extra Tools',
     'website': 'https://odoo-community.org/',
     'author': 'LasLabs, '
