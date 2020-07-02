1a466ef41bbc3a8518462bc26c276cdd89f5d888
Denis Roussel <denis.roussel@acsone.eu>
2020-07-02 09:21:54 +0200

    [10.0][IMP] mass_editing: Lower the wizard code and allow override

--- mass_editing/wizard/mass_editing_wizard.py
+++ mass_editing/wizard/mass_editing_wizard.py
@@ -11,6 +11,131 @@ from odoo import api, models
 class MassEditingWizard(models.TransientModel):
     _name = 'mass.editing.wizard'
 
+    def __init__(self):
+        super(MassEditingWizard, self).__init__()
+        self._all_fields = {}
+
+    @api.model
+    def _get_action_selection(self, field):
+        selection = [('set', 'Set')]
+        if field.ttype == "many2many":
+            selection.append(('remove_m2m', 'Remove'))
+        else:
+            selection.append(('remove', 'Remove'))
+
+    @api.model
+    def _build_action_field(self, field, field_info):
+        """
+        Build the 'Add/Remove...' selection
+        :param field:
+        :param field_info:
+        :return:
+        """
+        self._all_fields["selection__" + field.name] = {
+            'type': 'selection',
+            'string': field_info[field.name]['string'],
+            'selection': self._get_action_selection(field)
+        }
+
+    def _get_field_options(self, field, field_info):
+        """
+        Get field specific options
+        :param field:
+        :param field_info:
+        :return:
+        """
+        options = {
+            'type': field.ttype,
+            'string': field.field_description,
+            'views': {},
+        }
+        if field.ttype == "many2many":
+            options = field_info[field.name]
+        elif field.ttype == "char":
+            options.update({'size': field.size or 256})
+        elif field.ttype == "selection":
+            options.update({'selection': field_info[field.name]['selection']})
+        elif field.ttype in ("many2one", "one2many"):
+            options.update({'relation': field.relation})
+        return options
+
+    def _build_field(self, field, field_info):
+        """
+        Build field itself
+        :param field:
+        :param field_info:
+        :return:
+        """
+        self._all_fields[field.name] = self._get_field_options(
+            field, field_info)
+
+    def _get_values_xml_options(self, field):
+        options = {
+            'name': field.name,
+            'colspan': '4',
+            'nolabel': '1',
+            'attrs': ("{'invisible':[('selection__" +
+                      field.name + "', '=', 'remove')]}"),
+        }
+        if field.ttype == "many2many":
+            options.update({
+                'colspan': '6',
+                'attrs': ("{'invisible': [('selection__" + field.name + "', '=', 'remove_m2m')]}"),})
+        elif field.ttype in ("one2many", "text"):
+            options.update({'colspan': '6'})
+        return options
+
+    def _get_action_xml_options(self, field):
+        options = {
+            'colspan': '2',
+            'nolabel': '1',
+            'name': "selection__" + field.name,
+        }
+        if field.ttype in ("many2many", "text"):
+            options.update({'colspan': '6'})
+        elif field.ttype == "one2many":
+            options.update({'colspan': '4'})
+
+    def _build_xml(self, field, xml_group):
+        """
+        Build the sub xml elements from parent node
+        For many2many and text ones, build them inside another group
+        :param field:
+        :param field_info:
+        :param xml_group:
+        :return:
+        """
+        if field.ttype in ("many2many", "text"):
+            xml_group = etree.SubElement(xml_group, "group", {
+                'colspan': '6',
+                'col': '6',
+            })
+            etree.SubElement(xml_group, 'separator', {
+                'string': self._all_fields[field.name]['string'],
+                'colspan': '6',
+            })
+        etree.SubElement(
+            xml_group, 'field', self._get_action_xml_options(field))
+        etree.SubElement(
+            xml_group, 'field', self._get_values_xml_options(field))
+
+
+    @api.model
+    def _build_fields(self, field, field_info, xml_group):
+        """
+        Build the wizard form :
+           * Build field itself
+           * Build the action field (Set/Remove...)
+           * Build the xml view
+        :param field:
+        :param field_info:
+        :param xml_group:
+        :return:
+        """
+        self._build_field(field, field_info)
+        self._build_action_field(field, field_info)
+        self._build_xml(field, xml_group)
+
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
                         submenu=False):
@@ -23,7 +148,6 @@ class MassEditingWizard(models.TransientModel):
         if context.get('mass_editing_object'):
             mass_obj = self.env['mass.object']
             editing_data = mass_obj.browse(context.get('mass_editing_object'))
-            all_fields = {}
             xml_form = etree.Element('form', {
                 'string': tools.ustr(editing_data.name)
             })
@@ -42,174 +166,7 @@ class MassEditingWizard(models.TransientModel):
             model_obj = self.env[context.get('active_model')]
             field_info = model_obj.fields_get()
             for field in editing_data.field_ids:
-                if field.ttype == "many2many":
-                    all_fields[field.name] = field_info[field.name]
-                    all_fields["selection__" + field.name] = {
-                        'type': 'selection',
-                        'string': field_info[field.name]['string'],
-                        'selection': [('set', 'Set'),
-                                      ('remove_m2m', 'Remove'),
-                                      ('add', 'Add')]
-                    }
-                    xml_group = etree.SubElement(xml_group, 'group', {
-                        'colspan': '6',
-                        'col': '6',
-                    })
-                    etree.SubElement(xml_group, 'separator', {
-                        'string': field_info[field.name]['string'],
-                        'colspan': '6',
-                    })
-                    etree.SubElement(xml_group, 'field', {
-                        'name': "selection__" + field.name,
-                        'colspan': '6',
-                        'nolabel': '1'
-                    })
-                    etree.SubElement(xml_group, 'field', {
-                        'name': field.name,
-                        'colspan': '6',
-                        'nolabel': '1',
-                        'attrs': ("{'invisible': [('selection__" +
-                                  field.name + "', '=', 'remove_m2m')]}"),
-                    })
-                elif field.ttype == "one2many":
-                    all_fields["selection__" + field.name] = {
-                        'type': 'selection',
-                        'string': field_info[field.name]['string'],
-                        'selection': [('set', 'Set'), ('remove', 'Remove')],
-                    }
-                    all_fields[field.name] = {
-                        'type': field.ttype,
-                        'string': field.field_description,
-                        'relation': field.relation,
-                    }
-                    etree.SubElement(xml_group, 'field', {
-                        'name': "selection__" + field.name,
-                        'colspan': '4',
-                    })
-                    etree.SubElement(xml_group, 'field', {
-                        'name': field.name,
-                        'colspan': '6',
-                        'nolabel': '1',
-                        'attrs': ("{'invisible':[('selection__" +
-                                  field.name + "', '=', 'remove_o2m')]}"),
-                    })
-                elif field.ttype == "many2one":
-                    all_fields["selection__" + field.name] = {
-                        'type': 'selection',
-                        'string': field_info[field.name]['string'],
-                        'selection': [('set', 'Set'), ('remove', 'Remove')],
-                    }
-                    all_fields[field.name] = {
-                        'type': field.ttype,
-                        'string': field.field_description,
-                        'relation': field.relation,
-                    }
-                    etree.SubElement(xml_group, 'field', {
-                        'name': "selection__" + field.name,
-                        'colspan': '2',
-                    })
-                    etree.SubElement(xml_group, 'field', {
-                        'name': field.name,
-                        'nolabel': '1',
-                        'colspan': '4',
-                        'attrs': ("{'invisible':[('selection__" +
-                                  field.name + "', '=', 'remove')]}"),
-                    })
-                elif field.ttype == "char":
-                    all_fields["selection__" + field.name] = {
-                        'type': 'selection',
-                        'string': field_info[field.name]['string'],
-                        'selection': [('set', 'Set'), ('remove', 'Remove')],
-                    }
-                    all_fields[field.name] = {
-                        'type': field.ttype,
-                        'string': field.field_description,
-                        'size': field.size or 256,
-                    }
-                    etree.SubElement(xml_group, 'field', {
-                        'name': "selection__" + field.name,
-                        'colspan': '2',
-                    })
-                    etree.SubElement(xml_group, 'field', {
-                        'name': field.name,
-                        'nolabel': '1',
-                        'attrs': ("{'invisible':[('selection__" +
-                                  field.name + "','=','remove')]}"),
-                        'colspan': '4',
-                    })
-                elif field.ttype == 'selection':
-                    all_fields["selection__" + field.name] = {
-                        'type': 'selection',
-                        'string': field_info[field.name]['string'],
-                        'selection': [('set', 'Set'), ('remove', 'Remove')]
-                    }
-                    etree.SubElement(xml_group, 'field', {
-                        'name': "selection__" + field.name,
-                        'colspan': '2',
-                    })
-                    etree.SubElement(xml_group, 'field', {
-                        'name': field.name,
-                        'nolabel': '1',
-                        'colspan': '4',
-                        'attrs': ("{'invisible':[('selection__" +
-                                  field.name + "', '=', 'remove')]}"),
-                    })
-                    all_fields[field.name] = {
-                        'type': field.ttype,
-                        'string': field.field_description,
-                        'selection': field_info[field.name]['selection'],
-                    }
-                else:
-                    all_fields[field.name] = {
-                        'type': field.ttype,
-                        'string': field.field_description,
-                    }
-                    all_fields["selection__" + field.name] = {
-                        'type': 'selection',
-                        'string': field_info[field.name]['string'],
-                        'selection': [('set', 'Set'), ('remove', 'Remove')]
-                    }
-                    if field.ttype == 'text':
-                        xml_group = etree.SubElement(xml_group, 'group', {
-                            'colspan': '6',
-                            'col': '6',
-                        })
-                        etree.SubElement(xml_group, 'separator', {
-                            'string': all_fields[field.name]['string'],
-                            'colspan': '6',
-                        })
-                        etree.SubElement(xml_group, 'field', {
-                            'name': "selection__" + field.name,
-                            'colspan': '6',
-                            'nolabel': '1',
-                        })
-                        etree.SubElement(xml_group, 'field', {
-                            'name': field.name,
-                            'colspan': '6',
-                            'nolabel': '1',
-                            'attrs': ("{'invisible':[('selection__" +
-                                      field.name + "','=','remove')]}"),
-                        })
-                    else:
-                        all_fields["selection__" + field.name] = {
-                            'type': 'selection',
-                            'string': field_info[field.name]['string'],
-                            'selection': [('set', 'Set'), ('remove', 'Remove')]
-                        }
-                        etree.SubElement(xml_group, 'field', {
-                            'name': "selection__" + field.name,
-                            'colspan': '2',
-                        })
-                        etree.SubElement(xml_group, 'field', {
-                            'name': field.name,
-                            'nolabel': '1',
-                            'attrs': ("{'invisible':[('selection__" +
-                                      field.name + "','=','remove')]}"),
-                            'colspan': '4',
-                        })
-            # Patch fields with required extra data
-            for field in all_fields.values():
-                field.setdefault("views", {})
+                self._build_field(field, field_info, xml_group)
             etree.SubElement(xml_form, 'separator', {
                 'string': '',
                 'colspan': '6',
@@ -229,14 +186,15 @@ class MassEditingWizard(models.TransientModel):
             })
             root = xml_form.getroottree()
             result['arch'] = etree.tostring(root)
-            result['fields'] = all_fields
+            result['fields'] = self._all_fields
         return result
 
     @api.model
     def create(self, vals):
-        if (self._context.get('active_model') and
-                self._context.get('active_ids')):
-            model_obj = self.env[self._context.get('active_model')]
+        active_model = self._context.get('active_model')
+        active_ids = self._context.get('active_ids')
+        if active_model and active_ids:
+            model_obj = self.env[active_model]
             model_field_obj = self.env['ir.model.fields']
             translation_obj = self.env['ir.translation']
             values = {}
@@ -250,15 +208,14 @@ class MassEditingWizard(models.TransientModel):
                         # If field to remove is translatable,
                         # its translations have to be removed
                         model_field = model_field_obj.search([
-                            ('model', '=', self._context.get('active_model')),
+                            ('model', '=', active_model),
                             ('name', '=', split_key)])
                         if model_field and model_field.translate:
                             translation_ids = translation_obj.search([
-                                ('res_id', 'in', self._context.get(
-                                    'active_ids')),
+                                ('res_id', 'in', active_ids),
                                 ('type', '=', 'model'),
                                 ('name', '=', u"{0},{1}".format(
-                                    self._context.get('active_model'),
+                                    active_model,
                                     split_key))])
                             translation_ids.unlink()
 
@@ -270,7 +227,7 @@ class MassEditingWizard(models.TransientModel):
                             m2m_list.append((4, m2m_id))
                         values.update({split_key: m2m_list})
             if values:
-                model_obj.browse(self._context.get('active_ids')).write(values)
+                model_obj.browse(active_ids).write(values)
         return super(MassEditingWizard, self).create({})
 
     @api.multi
