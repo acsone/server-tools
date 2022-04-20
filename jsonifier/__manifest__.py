1d4c06e4a2ea61f89c72125c9a823ba32e06da62
Simone Orsi <simahawk@gmail.com>
2022-04-20 13:31:57 +0200

    Replace base_jsonify w/ jsonifier
    Rationale:
    
    1. the name reflects better what it does
       as this is not _just_ a base module
    
    2. solves publication issue on odoo apps store
       due to an old module registered w/ the same name
       that even if un-published blocks publishing the module
       for all versions.
       This in turn, blocks the publication of ALL modules
       that depend on base_jsonify.

--- jsonifier/__manifest__.py
+++ jsonifier/__manifest__.py
@@ -4,19 +4,23 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "Base JSONify",
-    "summary": """
-    Base module that provide the jsonify method on all models.
-
-    WARNING: since version 14.0.2.0.0 the module have been renamed to `jsonifier`.
-    This module now depends on it only for backward compatibility.
-    It will be discarded in v15 likely.
-    """,
-    "version": "14.0.2.0.0",
+    "name": "JSONifier",
+    "summary": "JSON-ify data for all models",
+    "version": "14.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/server-tools",
     "author": "Akretion, ACSONE, Camptocamp, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "installable": True,
-    "depends": ["jsonifier"],
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ir_exports_view.xml",
+        "views/ir_exports_resolver_view.xml",
+    ],
+    "demo": [
+        "demo/resolver_demo.xml",
+        "demo/export_demo.xml",
+        "demo/ir.exports.line.csv",
+    ],
 }
