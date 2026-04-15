dd3aa3bbb05baed318bd14a4366a5a4b8ec127ea
Quentin Groulard <quentin.groulard@acsone.eu>
2026-04-15 18:00:23 +0200

    [ADD] base_trusted_proxies
    Allow configuration of the number of trusted proxies for accurate Client IP detection.

---
+++ base_trusted_proxies/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Base Trusted Proxies",
+    "summary": """
+        Allow configuration of the number of trusted proxies
+        for accurate Client IP detection.
+    """,
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [],
+    "post_load": "post_load",
+}
